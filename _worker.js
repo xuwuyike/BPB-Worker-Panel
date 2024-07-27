4309907792eda6a7a7bc31fd26a9d05c9ea5122c
xuwuyike <133773496+xuwuyike@users.noreply.github.com>
2024-07-27 22:06:38 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '77f9d8ca-4693-4700-b40b-d8c7d4906e0e';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
